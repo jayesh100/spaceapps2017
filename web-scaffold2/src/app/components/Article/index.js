e7cb7912c20e57f20e4dbd205d5cff621aca44b5
dylanzt <dylanzt@gmail.com>
2017-04-30 11:32:28 +1000

    Prepend "source: " to source

--- web-scaffold2/src/app/components/Article/index.js
+++ web-scaffold2/src/app/components/Article/index.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 
 import styles from './styles.styl';
 
+const source = `source: ${this.article.source}`;
+
 export default class Article extends Component {
   static propTypes = {
     article: {},
@@ -22,7 +24,7 @@ export default class Article extends Component {
           <div className={styles.sid}>{this.article.sid}</div>
         </div>
         <p className={styles.summary}>{this.article.summary}</p>
-        <div className={styles.source}>{this.article.source}</div>
+        <div className={styles.source}>{source}</div>
       </div>
     );
   }
