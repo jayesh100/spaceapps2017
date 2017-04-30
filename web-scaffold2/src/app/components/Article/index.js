811a1c2d35639d3313cac36c96cc82f48678d5fc
dylanzt <dylanzt@gmail.com>
2017-04-30 12:05:26 +1000

    Fix Id naming

--- web-scaffold2/src/app/components/Article/index.js
+++ web-scaffold2/src/app/components/Article/index.js
@@ -15,7 +15,7 @@ export default class Article extends Component {
       <div className={styles.article}>
         <div className={styles.header}>
           <h3 className={styles.title}>{this.props.article.title}</h3>
-          <div className={styles.sid}>{this.props.article.sid}</div>
+          <div className={styles.sid}>{this.props.article.sId}</div>
         </div>
         <p className={styles.summary}>{this.props.article.summary}</p>
         <div className={styles.source}>{`Source: ${this.props.article.source}`}</div>
